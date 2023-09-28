18ceec545bcea77a1a5dae7835226814e3fb0799
pandonline <geffroy250@gmail.com>
2023-09-28 17:11:17 +0200

    version non aboutit (changement de support de dev)

---
+++ anchor.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scale = Vector3(0.01,0.01,0.01)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
