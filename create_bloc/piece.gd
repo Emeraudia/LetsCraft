e72c219b900b08764de3cfe3e49251cc1fb8c469
Pandonline <95920019+Pandonline@users.noreply.github.com>
2023-09-30 13:49:57 +0200

    Merge avec develop
    Fusion du concept de piece avec le syteme de mouvement.
    
    Modification des codes liée à la pièce et son changement de concept

--- create_bloc/piece.gd
+++ create_bloc/piece.gd
@@ -1,9 +1,14 @@
 extends Node3D
+signal clicked(node) #signal emit when you click on the object
 
+var enter = false
+var origin = position
+var newMaterial = StandardMaterial3D.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	newMaterial.albedo_color = Color(0.42, 0.69, 0.13, 1.0)
+	$ObjetListe/Obj/Mesh.material_override = newMaterial
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,6 +18,9 @@ func _process(delta):
 		antene_modif($ObjetListe.scale)
 	if(Input.is_action_just_pressed("resize")):
 		resize()
+		
+	if(enter && Input.is_action_just_pressed("click_gauche")):
+		emit_signal("clicked",self)
 		
 func anchor_modif(piece_size):
 	for decay in $AnchorListe.get_children():
@@ -76,6 +84,30 @@ func resize():
 	antene_modif($ObjetListe.scale)
 	
 	
+	
+func _on_mouse_entered():
+	enter = true
+
+
+
+func _on_mouse_exited():
+	enter = false
+	
+#change the color of the mesh
+#@param a color
+func change_color(c):
+	newMaterial.albedo_color = c
+	$ObjetListe/Obj/Mesh.material_override = newMaterial
+	
+#relocalise la piece a la positon p
+func setPos(p):
+	position.x = position.x+p[0]
+	position.y = position.y+p[1]
+	position.z = position.z+p[2]
+	
+	
+func getPosPlan():
+	return [position.x,position.y]
 	
 
 
