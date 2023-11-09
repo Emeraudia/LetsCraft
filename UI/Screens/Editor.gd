a127118815d248eb0d338efe1ca0bf69899d1e11
Caeles12 <pol.jaouen@gmail.com>
2023-11-09 10:08:42 +0100

    Bouton camera

--- UI/Screens/Editor.gd
+++ UI/Screens/Editor.gd
@@ -5,12 +5,14 @@ signal mode
 func _on_select_button_pressed():
 	mode.emit("select")
 
-
 func _on_move_button_pressed():
 	mode.emit("move")
 
 func _on_new_cube_button_pressed():
 	mode.emit("new")
-
+	
 func _on_edit_shape_button_pressed():
 	mode.emit("edit")
+
+func _on_camera_button_pressed():
+	mode.emit("camera")
